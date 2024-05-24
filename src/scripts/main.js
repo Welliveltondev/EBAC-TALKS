4d6c9fedc7cdb929529e4e906b70abf7ed171739
Wellivelton Fonseca <welliveltonfonseca@gmail.com>
2024-05-24 13:05:19 -0300

    criando contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
-console.log("ola mundo");
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+    
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+
+
+
+    const diasATeOEvento = Math.floor (distanciaAteOEvento / diasEmMs);
+    const  horasAteOEvento = Math.floor ((distanciaAteOEvento % diasEmMs) / horasEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMs) / minutosEmMs);
+    const segundosSteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `em ${diasATeOEvento}d ${horasAteOEvento}h ${minutosAteOEventom} ${segundosSteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML =  'Evento expirado';
+    }
+}, 1000);
